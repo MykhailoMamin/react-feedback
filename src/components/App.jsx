9cc81a3d4c558431127282689d7ce6a9de37e144
MykhailoMamin <mishamamin2016@gmail.com>
2023-11-08 23:16:39 +0200

    finish feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import { Component } from 'react';
 
-class App {
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+
+class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
+  handleFeedback = e => {
+    return this.setState(prevState => ({
+      [e.target.id]: prevState[e.target.id] + 1,
+    }));
+  };
+
+  countTotalFeedback = () =>
+    this.state.good + this.state.neutral + this.state.bad;
+
+  countPositiveFeedbackPercentage = () => {
+    const counterPercentage = Math.round(
+      (100 * this.state.good) / this.countTotalFeedback()
+    );
+    if (isNaN(counterPercentage)) {
+      return 0;
+    }
+    return counterPercentage;
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <>
-        <FeedbackOptions options={['good', 'neutral', 'bad']} />
+        <section>
+          <h1>Please leave feedback</h1>
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleFeedback}
+          />
+
+          <h2>Statistics</h2>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback}
+            positivePercentage={this.countPositiveFeedbackPercentage}
+          />
+        </section>
       </>
     );
   }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,5 +1,22 @@
-const FeedbackOptions = ({ good, neutral, bad }) => {
-  return;
+import css from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul className={css.buttonItem}>
+      {options.map((option, index) => (
+        <li key={index} className={css.buttonList}>
+          <button
+            type="button"
+            className={css.button}
+            id={option}
+            onClick={e => onLeaveFeedback(e)}
+          >
+            {option}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default FeedbackOptions;
